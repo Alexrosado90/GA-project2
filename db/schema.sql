bafd78c424dfa7b108fbb3a737145f340bdb6476
Alex Rosado <Ace@Macbooks-MacBook-Pro-4.local>
2018-06-27 18:26:49 -0400

    created database

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE monthlyexpenses;
+
+\c monthlyexpenses
+
+DROP TABLE IF EXISTS people;
+DROP TABLE IF EXISTS bills;
+
+CREATE TABLE people (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    monthly_income INTEGER
+);
+
+CREATE TABLE bills (
+    id SERIAL PRIMARY KEY,
+    rent INTEGER,
+    electric INTEGER,
+    car INTEGER,
+    insurance INTEGER,
+    food INTEGER,
+    other INTEGER
+);
---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+\c monthlyexpenses
+
+DELETE FROM people;
+DELETE FROM bills;
+
+INSERT INTO people (name, age, monthly_income) VALUES ('Alex', 27, 3500);
