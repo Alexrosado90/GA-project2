4c1700a0ac582cf3eefc2098fbe937c7d537ea3a
Alex Rosado <Ace@Macbooks-MacBook-Pro-4.local>
2018-06-27 20:39:39 -0400

    added boilerplate code

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+const People = require('./models/people')
+const Bills = require('./models/bills')
+const moment = require('moment')
+const methodOverride = require('method-override')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(methodOverride('_method'))
+
+app.set('view engine', 'ejs')
+
+app.use('/public', express.static('public'))
+
+app.listen(port, function(){
+  console.log(`Listening on port ${port}`)
+});
